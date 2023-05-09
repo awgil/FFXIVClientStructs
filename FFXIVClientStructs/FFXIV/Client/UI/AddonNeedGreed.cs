d2e102bde711c4322c12b80d80fe8f1e01a5d268
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2023-05-09 21:38:53 +0100

    Fixed AddonNeedGreed layout
    - Items array can't have 32 elements, since it will make structure larger than can be seen from destructor
    - constructor fills the array starting at 0x220 rather than 0x228 and of size 16
    - there is some other pointer field immediately following array, assuming it starts at 0x220 and has length 16
    - to preserve ItemId offset from parent structure start, it needs its offset from substructure adjusted

--- FFXIVClientStructs/FFXIV/Client/UI/AddonNeedGreed.cs
+++ FFXIVClientStructs/FFXIV/Client/UI/AddonNeedGreed.cs
@@ -3,20 +3,20 @@
 namespace FFXIVClientStructs.FFXIV.Client.UI;
 
 [Addon("NeedGreed")]
-[StructLayout(LayoutKind.Explicit, Size = 0x2B8)]
+[StructLayout(LayoutKind.Explicit, Size = 0x510)]
 public unsafe partial struct AddonNeedGreed
 {
     [FieldOffset(0x00)] public AtkUnitBase AtkUnitBase;
-    
-    // Limit is assumed to be 32, due to the addon having 32 pre-allocated list item renderers
-    [FixedSizeArray<LootItemInfo>(32)] 
-    [FieldOffset(0x228)] public fixed byte Items[0x28 * 32];
+
+    // Addon has 32 pre-allocated list item renderers
+    [FixedSizeArray<LootItemInfo>(16)]
+    [FieldOffset(0x220)] public fixed byte Items[0x28 * 16];
     
     // Contains other information for each loot item,
     // Such as time remaining, and a pointer to what is probably the tooltip string
     [StructLayout(LayoutKind.Explicit, Size = 0x28)]
     public struct LootItemInfo
     {
-        [FieldOffset(0x00)] public uint ItemId;
+        [FieldOffset(0x08)] public uint ItemId;
     }
 }
