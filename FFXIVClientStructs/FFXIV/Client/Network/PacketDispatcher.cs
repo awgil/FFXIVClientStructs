154f40448da07f08b810d9a24e36ea707fbeb025
Andrew Gilewsky <andrew.gilewsky@gmail.com>
2024-11-11 23:35:55 +0000

    Moved stuff around.

--- FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
+++ FFXIVClientStructs/FFXIV/Client/Network/PacketDispatcher.cs
@@ -8,4 +8,10 @@ namespace FFXIVClientStructs.FFXIV.Client.Network;
 public unsafe partial struct PacketDispatcher {
     [MemberFunction("48 89 5C 24 ?? 56 48 83 EC ?? 48 8B 0D ?? ?? ?? ?? 48 8B F2")]
     public partial void HandleSocialPacket(nint packet);
+
+    [MemberFunction("E8 ?? ?? ?? ?? E9 ?? ?? ?? ?? 44 0F B6 4B ?? 4C 8D 43 18 0F B7 53 14")]
+    public static partial void HandleEventPlayPacket(ulong objectId, uint eventId, ushort state, ulong a4, uint* payload, byte payloadSize);
+
+    [MemberFunction("E8 ?? ?? ?? ?? EB 10 48 8B 0D")]
+    public static partial void SendEventCompletePacket(uint eventId, ushort stage, byte a3, uint* args, byte numArgs, void* a6);
 }
